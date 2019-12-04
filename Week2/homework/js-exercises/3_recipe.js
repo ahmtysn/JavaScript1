9a143a74692153b859e2d54ef5548c3b64638578
ahmtysn <ahmtysn14@gmail.com>
2019-12-04 18:17:03 +0100

    i changed property names at object without space and with beginning small letter. also i used Array.isArray method instead of typeof method

--- Week2/homework/js-exercises/3_recipe.js
+++ Week2/homework/js-exercises/3_recipe.js
@@ -3,13 +3,13 @@
 // 1. Declare a variable that holds an object (your meal recipe).
 // 2. Give the object 3 properties: a `title` (string), a `servings` (number) and an `ingredients` (array of strings) property.
 let recipe = {
-  "Meal Name": "Omelete",
-  Serves: 2,
-  Ingredients: ["4 eggs", "2 strips of bacon", "1 tsp salt/pepper"]
+  mealName: "Omelete",
+  serves: 2,
+  ingredients: ["4 eggs", "2 strips of bacon", "1 tsp salt/pepper"]
 };
 // 3. Log each property out seperately, using a loop (for, while or do/while)
 for (let prop in recipe) {
-  if (typeof recipe[prop] !== "object") {
+  if (!Array.isArray(recipe[prop])) {
     console.log(`${prop}:${recipe[prop]}`);
   } else {
     console.log(prop + ":");
